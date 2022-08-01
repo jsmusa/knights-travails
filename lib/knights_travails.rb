a556ad61d98b845d2322c5783193a4e4284c753e
Jojo <jonathansardeamusa@gmail.com>
2022-08-01 19:48:17 +0800

    Add bfs method

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,25 +1,53 @@
 class Board
-  def initialize(start)
+  def initialize
     @board = []
   end
 
   def make_cells 
-    (0..7).each do |x|
-      (0..7).each do |y|
+    for x in (0..7)
+      for y in (0..7)
         @board.push(Node.new([x, y]))
       end
     end
+
+    @board
   end
 
-  def get_neighbors(start)
+  def get_neighbors(node)
+    position = node.position
     array = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
-    neighbors = array.map { |pos| start + pos }
 
-    neighbors.select { |neighbor| neighbor >= [0, 0] && neighbor <= [7, 7]}
+    array = array.map { |elem| elem.map.with_index { |coordinate, i| coordinate + position[i]} }
+    
+    valid = array.select do |elem|
+      elem.none? { |num| num < 0 || num > 7 }
+    end
+
+    valid.each { |elem| node.add_neighbor(find_cell(elem)) }
   end
 
-  def find(array)
-    @board.each { |node| return node if node.position == array}
+  def find_cell(array)
+    @board[array[0] * 8 + array[1]]
+  end
+
+  def connect_nodes
+    @board.each {|node| get_neighbors(node)}
+  end
+
+  def bfs(start, end_pos)
+    start_node = find_cell(start)
+    queue = [start_node]
+    result = []
+
+    until queue.empty?
+      queue[0].neighbors.each { |node| queue.push(node).uniq! }
+      current_node = queue.shift
+      result.push(current_node)
+
+      break if current_node.position == end_pos
+    end
+
+    result
   end
 end
 
@@ -29,12 +57,27 @@ class Node
     @neighbors = []
   end
 
+  def add_neighbor(node)
+    @neighbors.push(node)
+  end
+
   def position
-    [@position[0], position[1]]
+    [@position[0], @position[1]]
+  end
+
+  def neighbors
+    @neighbors.clone
   end
 end
 
-def knight_moves(start, end)
-  knight = Knight.new(start)
+board = Board.new
+board.make_cells
+board.connect_nodes
 
+result = board.bfs([3, 3], [4, 3])
+
+def knight_moves(start_pos, end_pos)
+  board.bfs(start_pos, end_pos)
 end
+
+result.each { |node| print "#{node.position} \n"}
